d3a28e1320eb4219509677aafb2748b4415d643c
Aurora Walberg <aurora.walberg@inmeta.no>
2023-11-22 16:17:01 +0100

    update

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -13,6 +13,7 @@ export default function Event({ frontmatter }: Props) {
     <li className="my-6">
       <h3 className="text-lg font-medium decoration-dashed">{organizer}</h3>
       <a
+        target="_blank"
         href={link}
         className="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 hover:underline focus-visible:no-underline focus-visible:underline-offset-0"
       >
